ed78f07788f1f8098f1045b614519c2658ecd8ab
Brady McDonough <brady.mcd.codes@gmail.com>
2018-10-20 23:23:13 -0600

    First "working" condition, readthrough pending

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //
-
+// TODO: Getters, Casing anomalies, Tests, Documentation, HACK comments
 
 extern crate reqwest;
 extern crate base_url;
@@ -18,17 +18,19 @@ use base_url::BaseUrl;
 /// directives.
 pub enum Anomaly {
     /// Any comment stored alongside some context, either the rest of the line the comment was found on
-    /// or the line following
+    /// or the line following. Context strings may be observed twice if a block comment is placed above
+    /// a line with a directive and comment.
     Comment( String /*The comment*/, String /*The context*/ ),
-    /// Any rule which refers to a remote host to the robots.txt file
-    CrossHostRule( String, BaseUrl ),
     /// Rules whose names are not in the normal casing format, ie. "allow" rather than "Allow"
     Casing( String, BaseUrl ), // mostly here to guage if this type of error is common
-    /// A known directive associated with a User-agent not normally associated as such or vice versa.
-    /// ie. a Disallow rule outside of a User-agent or a Sitemap inside of one.
-    MissSectioned( String, BaseUrl ),
+    /// A Rule located outside of a User-agent section
+    OrphanRule( Rule ),
+    /// A User-agent line nested in another User-agent section which already contains one or more Rules
+    RecursedUserAgent( String /*The agent's name*/ ),
+    /// Any known directive not noramlly found in a User-agent section.
+    MissSectionedDirective( String, String ),
     /// Any directive which is unimplemented or otherwise unknown
-    UnknownDirective( String, Option< String > ),
+    UnknownDirective( String, String ),
     /// Any line which isn't in the standard format for a robots.txt file, ie. a line without a ':'
     /// separator which is not a comment
     UnknownFormat( String ),
@@ -70,13 +72,90 @@ enum State {
     Normal( RobotsParser ), //Any lines at the root level (those without a useragent association)
 }
 
+enum DirectiveResult<'a> {
+    Ok_UserAgent( String ),
+    Ok_Rule( Rule ),
+    Ok_Sitemap( &'a str ),
+    //Ok_RequestRate( u32 ),
+    //Ok_CrawlDelay( u32 ),
+    Unknown(),
+}
+
+fn parse_directive<'a>( directive: &str, argument: &'a str ) -> DirectiveResult< 'a > {
+    match directive {
+        "User-agent" => {
+            DirectiveResult::Ok_UserAgent( argument.to_string( ) )
+        }
+        "Disallow" => {
+            DirectiveResult::Ok_Rule( Rule::new( false, argument.to_string( ) ) )
+        }
+        "Allow" => {
+            DirectiveResult::Ok_Rule( Rule::new( true, argument.to_string( ) ) )
+        }
+        "Sitemap" => {
+            DirectiveResult::Ok_Sitemap( argument )
+        }
+        _ => {
+            DirectiveResult::Unknown()
+        }
+    }
+}
+
+impl Rule {
+
+    fn new( allowance: bool, path: String ) -> Rule {
+        match allowance {
+            true => {
+                if path == "*" {
+                    Rule::AllowAll( path )
+                } else {
+                    Rule::Allow( path )
+                }
+            }
+            false => {
+                if path == "*" {
+                    Rule::DisallowAll( path )
+                } else {
+                    Rule::Disallow( path )
+                }
+            }
+        }
+    }
+
+}
+
 impl UserAgent {
+
+    fn new( agent: String ) -> Self {
+        UserAgent{
+            names: vec!( agent.to_string( ) ),
+            rules: Vec::new( ),
+            anomalies: Vec::new( ),
+        }
+    }
+
+    fn is_empty( &self ) -> bool {
+        self.rules.is_empty( )
+    }
+
+    fn add_agent( &mut self, name: String ) {
+        if self.is_empty( ) {
+            self.names.push( name );
+        } else {
+            self.anomalies.push( Anomaly::RecursedUserAgent( name ) );
+        }
+    }
+
+    fn add_rule( &mut self, rule: Rule ) {
+        self.rules.push( rule );
+    }
+
     fn add_comment( &mut self, context: String, comment: String ) {
         self.anomalies.push( Anomaly::Comment( comment, context ) );
     }
 
-    fn add_anomaly( &mut self, line: String ) {
-        self.anomalies.push( Anomaly::UnknownFormat( line ) );
+    fn add_anomaly( &mut self, anomaly: Anomaly ) {
+        self.anomalies.push( anomaly );
     }
 }
 
@@ -121,6 +200,46 @@ impl R_State {
         }
     }
 
+    fn directive_line( self, directive: &str, argument: &str ) -> Self {
+
+        let mut user_agent;
+
+        match self {
+            R_State::Comment( mut u, s ) => {
+                let mut context = format!( "{}: {}", directive, argument );
+                u.add_comment( context, s.to_string( ) );
+                user_agent = u;
+            }
+            R_State::Normal( u ) => {
+                user_agent = u;
+            }
+        }
+
+        match parse_directive( directive, argument ) {
+            DirectiveResult::Ok_UserAgent( ua ) => {
+                user_agent.add_agent( ua );
+                R_State::Normal( user_agent )
+            }
+            DirectiveResult::Ok_Rule( r ) => {
+                user_agent.add_rule( r );
+                R_State::Normal( user_agent )
+            }
+            DirectiveResult::Unknown() => {
+                user_agent.add_anomaly(
+                    Anomaly::UnknownDirective( directive.to_string( ),
+                                               argument.to_string( ) )
+                );
+                R_State::Normal( user_agent )
+            }
+            _ => {
+                user_agent.add_anomaly(
+                    Anomaly::MissSectionedDirective( directive.to_string( ), argument.to_string( ) )
+                );
+                R_State::Normal( user_agent )
+            }
+        }
+    }
+
     fn anomaly( self, line: &str ) -> Self {
 
         match self {
@@ -129,7 +248,7 @@ impl R_State {
                 R_State::Normal( u )
             }
             R_State::Normal( mut u ) => {
-                u.add_anomaly( line.to_string( ) );
+                u.add_anomaly( Anomaly::UnknownFormat( line.to_string( ) ) );
                 R_State::Normal( u )
             }
         }
@@ -192,7 +311,6 @@ impl State {
 
         let mut robots;
 
-
         match self {
             State::Comment( mut r, s ) => {
                 let mut context = format!( "{}: {}", directive, argument );
@@ -203,37 +321,42 @@ impl State {
                 s = s.directive_line( directive, argument );
                 return State::Agent( r, s );
             }
-            State::Normal( mut r ) => {
+            State::Normal( r ) => {
                 robots = r;
             }
         }
 
-        //Now match the directive with known directives
-        match directive {
-            "User-agent" => {
-                return State::Agent(
-                    robots,
-                    R_State::Normal( UserAgent::new( argument ) ),
+        match parse_directive( directive, argument ) {
+            DirectiveResult::Ok_UserAgent( ua ) => {
+                State::Agent( robots, R_State::Normal( UserAgent::new( ua ) ) )
+            }
+            DirectiveResult::Ok_Rule( r ) => {
+                robots.add_anomaly( Anomaly::OrphanRule( r ) );
+                State::Normal( robots )
+            }
+            DirectiveResult::Ok_Sitemap( s ) => {
+                let mut sitemap_url = robots.host_url( );
+                sitemap_url.set_path( s );
+                robots.add_sitemap( sitemap_url );
+                State::Normal( robots )
+            }
+//            DirectiveResult::Ok_RequestRate( r ) => {}
+//            DirectiveResult::Ok_CrawlDelay( d ) = > {}
+            DirectiveResult::Unknown() => {
+                robots.add_anomaly(
+                    Anomaly::UnknownDirective( directive.to_string( ),
+                                               argument.to_string( ) )
                 );
+                State::Normal( robots )
             }
-            "Disallow" => {
-                robots.anomalies.push( Anomaly::MissSectioned(
-                    directive.to_string( ),
-                    robots.host_url( ).set_path( argument ),
-                ) );
-            }
-            "Allow" => {}
-            "Sitemap" => {}
         }
-
-        State::Normal( robots )
     }
 
     fn anomaly( self, line: &str ) -> Self {
         match self {
             State::Comment( mut r, s ) => {
                 r.add_comment( line.to_string( ), s.to_string( ) );
-                r.add_anomaly( s.to_string( ) );
+                r.add_unknown( s.to_string( ) );
                 State::Normal( r )
             }
             State::Agent( r, mut s ) => {
@@ -241,7 +364,7 @@ impl State {
                 State::Agent( r, s )
             }
             State::Normal( mut r ) => {
-                r.add_anomaly( line.to_string( ) );
+                r.add_unknown( line.to_string( ) );
                 State::Normal( r )
             }
         }
@@ -279,9 +402,21 @@ impl RobotsParser {
         self.agents.push( agent );
     }
 
-    fn add_anomaly( &mut self, line: String ) {
+    fn add_sitemap( &mut self, url: BaseUrl ) {
+        self.sitemaps.push( url );
+    }
+
+    fn add_anomaly( &mut self, anomaly: Anomaly ) {
+        self.anomalies.push( anomaly );
+    }
+
+    fn add_unknown( &mut self, line: String ) {
         self.anomalies.push( Anomaly::UnknownFormat( line ) );
     }
+
+    /***********
+     * Creation
+     ******/
 
     pub fn guess_robots_url( &self ) -> BaseUrl {
         let mut ret = self.host.clone( );
@@ -358,6 +493,14 @@ impl RobotsParser {
         }
 
         state.eof( )
+    }
+
+    /***********
+     * Getters
+     ******/
+
+    pub fn host_url( &self ) -> BaseUrl {
+        self.host.clone( )
     }
 }
 
